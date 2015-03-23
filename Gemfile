87ef24c59d3b35e38243a9c43a04f7b28e6f5a71
rabinprithvi <rabindranath.prithvi@gmail.com>
2015-03-23 16:29:43 +0530

    CMS integrated

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'rails_admin', '~> 0.6.7'
+gem 'comfortable_mexican_sofa', '~> 1.12.7'
 group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  comfy_route :cms_admin, :path => '/admin'
   mount RailsAdmin::Engine => '/super_admin', as: 'rails_admin'
   root to: 'visitors#index'
   devise_for :users
+   # Make sure this routeset is defined last
+  comfy_route :cms, :path => '/', :sitemap => false
 end
